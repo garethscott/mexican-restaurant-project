b92ccf2f09ca99510551f31f993babaab3c5fcfc
garethscott <59648675+garethscott@users.noreply.github.com>
2020-11-23 16:41:15 +0000

    Further development to the about section

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import AboutBgImage from '../images/about-image-small.png'
+import AboutBgImage from '../images/about-image-small.png';
+import AboutBgImageNoFade from '../images/about-image-small-nofade.png'
 
 const AboutSection = () => {
+    const [swapAboutImg, setSwapAboutImg] = useState(false);
+    const [showAboutImg, setShowAboutImg] = useState(false);
+
+    const handleShowAboutImg = () => {
+        if (window.scrollY > 50) {
+            setShowAboutImg(true);
+        } else if (window.scrollY < 10) {
+            setShowAboutImg(false);
+        }
+        console.log(window.scrollY);
+        // console.log(showAboutImg)
+    }
+
+    window.addEventListener('scroll', handleShowAboutImg);
+
+
+
     return (
-        <AboutContainer>
+        <AboutContainer showAboutImg={showAboutImg}>
 
         </AboutContainer>
     )
@@ -15,9 +33,13 @@ export default AboutSection;
 const AboutContainer = styled.div`
   width: 100%;
   height: 907px;
-  background: url(${AboutBgImage});
+  background: url(${({ showAboutImg }) => (showAboutImg ? AboutBgImage : AboutBgImageNoFade)});
   background-position: center;
-  margin-top: -20px;
+  margin-top: 0px;
+  
+  ${({ showAboutImg }) => showAboutImg && `
+    margin-top: -20px;
+  `}
 `;
 
 
