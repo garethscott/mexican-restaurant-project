b7fed8e44c94ce8f1d9314c168860f3caf4f11e3
garethscott <59648675+garethscott@users.noreply.github.com>
2020-11-24 11:26:55 +0000

    Further development to the about and landing pages

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,29 +2,44 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import AboutBgImage from '../images/about-image-small.png';
 import AboutBgImageNoFade from '../images/about-image-small-nofade.png'
+import LgAboutImg from '../images/lg-about-img.png';
+import LgAboutImgNoFade from '../images/lg-about-img-nofade.png';
 
 const AboutSection = () => {
     const [swapAboutImg, setSwapAboutImg] = useState(false);
     const [showAboutImg, setShowAboutImg] = useState(false);
+    const [swapLargeFade, setSwapLargeFade] = useState(false);
 
     const handleShowAboutImg = () => {
         if (window.scrollY > 50) {
             setShowAboutImg(true);
+            setSwapLargeFade(true);
         } else if (window.scrollY < 10) {
             setShowAboutImg(false);
+            setSwapLargeFade(false);
         }
-        console.log(window.scrollY);
-        // console.log(showAboutImg)
     }
 
     window.addEventListener('scroll', handleShowAboutImg);
 
+    const handleSwapBgImg = () => {
+        if (window.innerWidth > 1795 || window.innerHeight > 1010) {
+            setSwapAboutImg(true);
+        } else {
+            setSwapAboutImg(false);
+        }
+    }
 
+
+    window.addEventListener('resize', handleSwapBgImg);
 
     return (
-        <AboutContainer showAboutImg={showAboutImg}>
+        <AboutContainer
+            showAboutImg={showAboutImg}
+            swapAboutImg={swapAboutImg}
+            swapLargeFade={swapLargeFade}>
 
-        </AboutContainer>
+        </AboutContainer >
     )
 }
 
@@ -39,6 +54,13 @@ const AboutContainer = styled.div`
   
   ${({ showAboutImg }) => showAboutImg && `
     margin-top: -20px;
+  `}
+
+  ${({ swapAboutImg }) => swapAboutImg && `
+  background: url(${LgAboutImgNoFade});
+  width: 100%;
+  height: 1296px;
+    // background: url(${({ swapLargeFade }) => (swapLargeFade ? LgAboutImg : LgAboutImgNoFade)});
   `}
 `;
 
